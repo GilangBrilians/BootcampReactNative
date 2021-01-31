cb64f6051c096650f73846a022d79db94fd4d6aa
GilangBrilians <gilangbrilians.f@gmail.com>
2021-01-31 17:14:37 +0700

    quiz 3

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import YoutubeUI from './Tugas/Tugas_12/App';
+import Quiz3 from './Quiz3';
+import YoutubeUI from './Tugas/Tugas_12/App'
 
 export default function App() {
   return (
-    <YoutubeUI />
+    <View >
+      <Quiz3 />
+    </View>
   );
 }
-
